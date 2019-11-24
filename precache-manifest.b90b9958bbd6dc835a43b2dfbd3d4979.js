f5568f4ca7be488853d39d6defb450ce2639af1c
iStaha <tahalatief@email.com>
2019-11-24 16:04:13 +0800

    Updates

---
+++ precache-manifest.b90b9958bbd6dc835a43b2dfbd3d4979.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0144b11a176bd718dcb04069fcbf6a1f",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "2018eeae79d002479d52",
+    "url": "/crwn-clothing/static/css/main.a5d9165f.chunk.css"
+  },
+  {
+    "revision": "10f9f1c41c45274fdf63",
+    "url": "/crwn-clothing/static/js/2.0c7f07f7.chunk.js"
+  },
+  {
+    "revision": "2018eeae79d002479d52",
+    "url": "/crwn-clothing/static/js/main.1e349d1f.chunk.js"
+  },
+  {
+    "revision": "10f85b2b9463efe22e26",
+    "url": "/crwn-clothing/static/js/runtime-main.910832d0.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
